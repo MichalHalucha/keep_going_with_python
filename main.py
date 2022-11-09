79565fed6fdfc72d63b3e9907e4f8b7fb5cd7a47
halucha <syncmasterpy@gmail.com>
2022-11-09 21:13:46 +0100

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import acceptable_Password_III,acceptable_Password_I, is_even, backward_String,acceptable_Password_II,first_word,fizz_buzz_if_div_by,number_length
+import goes_right_after,acceptable_Password_III,acceptable_Password_I, is_even, backward_String,acceptable_Password_II,first_word,fizz_buzz_if_div_by,number_length
 
 if __name__ == '__main__':
     print('All_missions_checkio')
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     fizz_buzz_if_div_by()
     number_length()
     acceptable_Password_III()
+    goes_right_after()
