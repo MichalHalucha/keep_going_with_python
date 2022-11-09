642ba7fef67328c595510f8a98efca2be0999af2
halucha <syncmasterpy@gmail.com>
2022-11-09 22:13:52 +0100

    Three_Words.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import goes_right_after,acceptable_Password_III,acceptable_Password_I, is_even, backward_String,acceptable_Password_II,first_word,fizz_buzz_if_div_by,number_length
+import Three_Words,goes_right_after,acceptable_Password_III,acceptable_Password_I, is_even, backward_String,acceptable_Password_II,first_word,fizz_buzz_if_div_by,number_length
 
 if __name__ == '__main__':
     print('All_missions_checkio')
@@ -11,3 +11,4 @@ if __name__ == '__main__':
     number_length()
     acceptable_Password_III()
     goes_right_after()
+    Three_Words()
